7ef2b7b38ede34094f7c9b6eb6e9774319e47fd0
Tamanaco Martínez <tamanacomb@gmail.com>
2021-09-18 21:10:43 -0400

    Beautify

--- projetFinalIntegrationWeb2/service-worker.js
+++ projetFinalIntegrationWeb2/service-worker.js
@@ -9,26 +9,26 @@ const FILES_TO_CACHE = [
     "discographie.html",
     "marchandise.html",
     "divers.html",
-    
+
 
 
 ];
 
 self.addEventListener('install', (evt) => {
-   console.log('[ServiceWorker] Install');
-   // Precache static resources here.
-   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-    })
+    console.log('[ServiceWorker] Install');
+    // Precache static resources here.
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
-   self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
-   console.log('[ServiceWorker] Activate');
-   //Remove previous cached data from disk.
+    console.log('[ServiceWorker] Activate');
+    //Remove previous cached data from disk.
     evt.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
@@ -36,23 +36,22 @@ self.addEventListener('activate', (evt) => {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
-            })
+            }));
+        })
     );
-    })
-);
-   self.clients.claim();
+    self.clients.claim();
 });
 
 
 self.addEventListener('fetch', (evt) => {
-   console.log('[ServiceWorker] Fetch', evt.request.url);
-   //Add fetch event handler here.
-   if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
     }
     evt.respondWith(
-    fetch(evt.request)
+        fetch(evt.request)
         .catch(() => {
             return caches.open(CACHE_NAME)
                 .then((cache) => {
@@ -68,12 +67,9 @@ self.addEventListener('fetch', (evt) => {
 // Register service worker.
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js')
-          .then((reg) => {
-            console.log('Service worker registered.', reg);
-          });
+        navigator.serviceWorker.register('service-worker.js')
+            .then((reg) => {
+                console.log('Service worker registered.', reg);
+            });
     });
-  }
-  
-
-
+}
